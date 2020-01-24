684f985a09e79f166401b57281e80d8424e72e2a
Davidgn98 <davidgn632@gmail.com>
2020-01-24 16:30:32 +0100

    Añadido el metodo main

---
+++ Main.java
@@ -0,0 +1,30 @@
+
+/**
+ * Clase que contiene el metodo main por el cual se inicia el programa
+ * 
+ * @author (David) 
+ * @version (24.1.2020 1.0v)
+ */
+public class Main
+{
+    
+
+    /**
+     * Constructor de la clase main
+     */
+    public Main()
+    {
+        
+    }
+
+    /**
+     * Metodo main que inicia el programa
+     * 
+     * @param  args   Un array de Strings para que funcione el metodo main 
+     */
+    public static void main(String[] args)
+    {
+        Lista objeto = new Lista();
+        objeto.menu();
+    }
+}
